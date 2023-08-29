2c93e60f67cdbffc4ea471c207ad0cbf888a34f3
Eslam Alawamy <eslamalawamy.com@gmail.com>
2023-08-29 09:44:23 -0700

    2

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,12 +1,12 @@
 #include "binary_trees.h"
 /**
- * binary_tree_insert_left - the main func
+ * binary_tree_insert_right - the main func
  * @parent: var 1
  * @value: var 2
  *
  * Return: the new left node
  */
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t new_node;
 
@@ -21,13 +21,13 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	}
 	new_node->parent = parent;
 	new_node->n = value;
-	new_node->left = parent->left;
-	new_node-right = NULL;
-	if (parent->left != NULL)
+	new_node->left = NULL;
+	new_node-right = parent->right;
+	if (parent->right != NULL)
 	{
-		parent->left->parent = new_node;
+		parent->right>parent = new_node;
 	}
-	parent->left = new_node;
+	parent->right = new_node;
 
 	return (new);
 }
